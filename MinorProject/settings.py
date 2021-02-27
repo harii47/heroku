aeedaffa1c16b2c51d2decd8975a2349cff3b4db
Harii47 <hariibabu47@gmail.com>
2021-02-27 17:14:40 +0530

    -

--- MinorProject/settings.py
+++ MinorProject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import dj_database_url
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -160,7 +161,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-
+# import dj_database_url
+# DATABASES = {
+#     'default': dj_database_url.config()
+# }
+# ##
+# django_heroku.settings(locals())
 
 
 
